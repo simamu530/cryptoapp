8ce454de20b98e7fae5d082015d05ce1cf5155a9
simamu530 <58772879+simamu530@users.noreply.github.com>
2021-05-17 11:27:02 +0900

    first

---
+++ src/components/Coinlist.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <h2>{{ name }}</h2>
+  <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Coinlist",
+  props: ["name","description"],
+}
+</script>
+
+<style scoped>
+h2 {
+  color: FFFFFE;
+}
+</style>
---
+++ src/views/Cryptolist.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="list-container">
+    <div class="list-title">
+      <p>銘柄一覧</p>
+    </div>
+    <div class="card-wrapper">
+      <div class="list-card">
+        <Coinlist name = "BTC" />
+        <Coinlist description = "ビットコイン（現物取引）" />
+        <button class="coin-detail">詳細</button>
+      </div>
+      <div class="list-card">
+        <Coinlist name = "BTC" />
+        <Coinlist description = "ビットコイン（現物取引）" />
+        <button class="coin-detail">詳細</button>
+      </div>
+      <div class="list-card">
+        <Coinlist name = "BTC" />
+        <Coinlist description = "ビットコイン（現物取引）" />
+        <button class="coin-detail">詳細</button>
+      </div>
+      <div class="list-card">
+        <Coinlist name = "BTC" />
+        <Coinlist description = "ビットコイン（現物取引）" />
+        <button class="coin-detail">詳細</button>
+      </div>
+      <div class="list-card">
+        <Coinlist name = "BTC" />
+        <Coinlist description = "ビットコイン（現物取引）" />
+        <button class="coin-detail">詳細</button>
+      </div>
+      <div class="list-card">
+        <Coinlist name = "BTC" />
+        <Coinlist description = "ビットコイン（現物取引）" />
+        <button class="coin-detail">詳細</button>
+      </div>
+      <div class="list-card">
+        <Coinlist name = "BTC" />
+        <Coinlist description = "ビットコイン（現物取引）" />
+        <button class="coin-detail">詳細</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Coinlist from "@/components/Coinlist.vue";
+
+export default {
+  name: "Cryptolist",
+  components: {
+    Coinlist,
+  },
+}
+</script>
+
+<style scoped>
+#list-container {
+background: #0F0E17;
+}
+.card-wrapper {
+  justify-content: space-between;
+}
+.list-card {
+  text-align: center;
+}
+.h2 {
+  color: FFFFFE;
+}
+</style>
